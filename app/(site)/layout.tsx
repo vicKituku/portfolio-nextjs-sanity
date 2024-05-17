195e4696c49497b854bd1fb2c7fe9cadd9420e00
VicKituku <victorkituku@gmail.com>
2024-05-17 20:20:06 +0300

    add insights and analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/global/Navbar";
 import Footer from "./components/global/Footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-zinc-900 text-white`}>
+        <Analytics />
+        <SpeedInsights />
         <Navbar />
         {children}
         <Footer />
